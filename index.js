c99996b2661a5c29ece4f7811cfa91402efec706
Hypermona <mohans8050@gmail.com>
2020-05-26 13:45:16 +0530

    new features: add to home

--- index.js
+++ index.js
@@ -33,6 +33,53 @@ window.addEventListener('load', ()=>{
             document.getElementById("kar-deaths-total").innerHTML = indianNumSystem(statewise[12].deaths);
         }
         );
+
+
+        // installation... setup...... start.......black-muted
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/sw.js').then(function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+
+    let deferredPrompt;
+    var div = document.querySelector('.add-to');
+    var button = document.querySelector('.add-to-btn');
+    div.style.display = 'none';
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        div.style.display = 'block';
+
+        button.addEventListener('click', (e) => {
+            // hide our user interface that shows our A2HS button
+            div.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice
+                .then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    } else {
+                        console.log('User dismissed the A2HS prompt');
+                    }
+                    deferredPrompt = null;
+                });
+        });
+    });
+        // installation..... setup..... end ....black-muted
+
+
         // All functions here...........................black-muted
         function indianNumSystem(x){
             x = x.toString();
