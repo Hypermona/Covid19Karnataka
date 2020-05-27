98bcd76abc58511d4ea6c23f5d62ec2b376772ce
Hypermona <mohans8050@gmail.com>
2020-05-27 14:00:01 +0530

    added cases district wise

--- index.js
+++ index.js
@@ -34,8 +34,37 @@ window.addEventListener('load', ()=>{
         }
         );
 
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            console.log("hello world");
+            console.log(data);
+            console.log("after ittiration");
+            const { Karnataka} =data;
+            let obj = Karnataka.districtData;
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    var val = obj[key];
+                    console.log(val);
+                    console.log(key, val.confirmed, val.delta.confirmed, val.active, val.recovered, val.delta.recovered, val.deceased, val.delta.deceased);
+                    addDistrict(key, val.confirmed, val.delta.confirmed, val.active, val.recovered, val.delta.recovered, val.deceased, val.delta.deceased);
+                }
+            }
+        });
+        // All click eventes...........................................red-muted
 
-
+        let button =document.getElementById('bt');
+        button.onclick = ()=>{
+            let displayData = document.getElementById('case-district-wise');
+            if (displayData.style.display =='none'){
+                displayData.style.display = 'initial';
+                bt.textContent = 'Show less';
+            }
+           else{
+                displayData.style.display = 'none';
+                bt.textContent = 'Cases in karnataka district wise';
+           }
+        }
         // All functions here...........................black-muted
         function indianNumSystem(x){
             x = x.toString();
@@ -56,6 +85,84 @@ window.addEventListener('load', ()=>{
             }
             return 0;
         }
-    }    
+    }
+    // adding district
+    function addDistrict(n,c,nc,a,r,nr,d,nd){
+        let caseDistrictWise = document.getElementById('case-district-wise');
+        console.log(caseDistrictWise);
+        let details = document.createElement('div');
+        details.classList.add("details","w3-animate-bottom");
+        let name = document.createElement('div');
+        name.classList.add('name','common');
+        name.innerHTML = n;
+
+        let confirmedDist = document.createElement('div');
+        confirmedDist.classList.add("confirmed-dist", "common", "align-dist", "common1");
+
+        let newCaseDist1 = document.createElement('p');
+        newCaseDist1.classList.add('new-case-dist-1', "text-danger",'cent');
+        newCaseDist1.innerHTML="&uarr;"+nc;
+        let totalDist1 = document.createElement('p');
+        totalDist1.classList.add('total-dist-1','cent');
+        totalDist1.innerHTML = c;
+
+        let activeDist = document.createElement('div');
+        activeDist.classList.add("active-dist", "common",  "align-dist" ,"common1");
+
+        let newCaseDist2 = document.createElement('p');
+        newCaseDist2.classList.add('new-case-dist-2', "text-primary",'cent');
+        newCaseDist2.innerHTML = "&uarr;" + nc ;
+        let totalDist2 = document.createElement('p');
+        totalDist2.classList.add('total-dist-2','cent');
+        totalDist2.innerHTML = a;
+
+        let recoveredDist = document.createElement('div');
+        recoveredDist.classList.add("recovered-dist", "common", "align-dist" ,"common1");
+
+        let newCaseDist3 = document.createElement('p');
+        newCaseDist3.classList.add('new-case-dist-3', "text-success",'cent');
+        newCaseDist3.innerHTML = "&uarr;" + nr ;
+        let totalDist3 = document.createElement('p');
+        totalDist3.classList.add('total-dist-3','cent');
+        totalDist3.innerHTML = r;
+
+        let deathsDist = document.createElement('div');
+        deathsDist.classList.add("deaths-dist", "common",  "align-dist" ,"common1");
+
+        let newCaseDist4 = document.createElement('p');
+        newCaseDist4.classList.add('new-case-dist-4', "text-secondary",'cent');
+        newCaseDist4.innerHTML = "&uarr;" + nd ;
+        let totalDist4 = document.createElement('p');
+        totalDist4.classList.add('total-dist-4','cent');
+        totalDist4.innerHTML = d;
+
+        // appending everything..............
+        if(caseDistrictWise != null){
+        confirmedDist.appendChild(newCaseDist1);
+        confirmedDist.appendChild(totalDist1);
+
+        activeDist.appendChild(newCaseDist2);
+        activeDist.appendChild(totalDist2);
+
+        recoveredDist.appendChild(newCaseDist3);
+        recoveredDist.appendChild(totalDist3);
+
+        deathsDist.appendChild(newCaseDist4);
+        deathsDist.appendChild(totalDist4);
+
+        details.appendChild(name);
+        details.appendChild(confirmedDist);
+        details.appendChild(activeDist);
+        details.appendChild(recoveredDist);
+        details.appendChild(deathsDist);
+
+        caseDistrictWise.appendChild(details);
+        }
+        else{
+            console.log("gertgh");
+        }
+    }
+    
+    
 
 });
